db28836c1e1639f1d515b6f08a29f0247038ec78
Doruk <dgorbon28@my.uaa.k12.tr>
2026-03-10 08:55:41 +0300

    Add files via upload

---
+++ Domino50A.java
@@ -0,0 +1,13 @@
+package Solutions;
+import java.util.*;
+
+public class Domino50A {
+
+	public static void main(String[] args) {
+		Scanner read= new Scanner(System.in);
+		int a = read.nextInt();
+		int b= read.nextInt();
+		System.out.println(a*b/2);
+	}
+
+}
